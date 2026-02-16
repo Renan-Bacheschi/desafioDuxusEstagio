35b553aee79be4539daf2a62d5c5571d885bf8de
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2026-02-16 18:33:29 -0300

    feat: implementa controller do método integrante-mais-usado.
    - Testes de chamada via api. fluxo da rota.
    - tratamento de exception com a classe PeriodoSemDadosException.
    - Map.of para melhor legibilidade.

--- src/main/java/br/com/duxusdesafio/controllers/IntegranteController.java
+++ src/main/java/br/com/duxusdesafio/controllers/IntegranteController.java
@@ -1,11 +1,19 @@
 package br.com.duxusdesafio.controllers;
 
 import br.com.duxusdesafio.dtos.IntegranteDTO;
+import br.com.duxusdesafio.exceptions.PeriodoSemDadosException;
 import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.model.Time;
 import br.com.duxusdesafio.repositories.IntegranteRepository;
+import br.com.duxusdesafio.repositories.TimeRepository;
+import br.com.duxusdesafio.service.ApiService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/integrantes")
@@ -13,10 +21,14 @@ import org.springframework.web.bind.annotation.*;
 public class IntegranteController {
 
     private final IntegranteRepository repository;
+    private final TimeRepository timeRepository;
+    private final ApiService apiService;
 
     // Injeção via construtor, SOLID
-    public IntegranteController(IntegranteRepository repository) {
+    public IntegranteController(IntegranteRepository repository, TimeRepository timeRepository, ApiService apiService) {
         this.repository = repository;
+        this.timeRepository = timeRepository;
+        this.apiService = apiService;
     }
 
     @PostMapping
@@ -28,5 +40,20 @@ public class IntegranteController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(repository.save(novoIntegrante));
+    }
+
+    @GetMapping("/mais-usado")
+    public ResponseEntity<Map<String, String>> getIntegranteMaisUsado(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicial,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFinal) {
+
+        List<Time> todosOsTimes = timeRepository.findAll();
+        Integrante maisUsado = apiService.integranteMaisUsado(dataInicial, dataFinal, todosOsTimes);
+
+        if (maisUsado == null) {
+            throw new PeriodoSemDadosException();
+        }
+
+        return ResponseEntity.ok(Map.of("integrante", maisUsado.getNome()));
     }
 }
