57f4d72e2b0fa82a1ab86e11456082f912c09230
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2026-02-20 02:32:12 -0300

    refactor: retira anotacao @NotBlank para permitir entrar da entidade nome time.

--- src/main/java/br/com/duxusdesafio/dtos/TimeRequestDTO.java
+++ src/main/java/br/com/duxusdesafio/dtos/TimeRequestDTO.java
@@ -1,13 +1,11 @@
 package br.com.duxusdesafio.dtos;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
 public record TimeRequestDTO(
-        @NotBlank(message = "O nome do time não pode ser vazio")
         String nome,
 
         @NotNull(message = "A data é obrigatória")
